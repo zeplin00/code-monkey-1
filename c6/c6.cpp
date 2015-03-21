fb3cd9bffda3adf68eb7371195d85307778cef8b
zeplin00 <zeplin00@outlook.com>
2015-03-21 12:41:54 -0700

    Updating c6

--- c6/c6.cpp
+++ c6/c6.cpp
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <iostream>
 
+// Pull in the array implementation of stack
+// 
 #include "array_stack.h"
 
-#include "stack_user.h"
+// Code that uses the abstract interface. 
+// This code might have existed before the array stack implememtation was written
+extern void use_gstack(Stack& s_ref);
 
 /* 
  * Abstract User Defined Types (Classes !!)
